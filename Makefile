a7c864d073f3f5abebd1a9f2dda88e3303f69c9c
ttaiclet <ttaiclet@ripe.net>
2025-07-23 16:29:30 +0200

    add: rebuild (dirty) target

--- Makefile
+++ Makefile
@@ -35,6 +35,9 @@ docker: ## Build Docker image
 build: ## Build OpenWRT
 	@./scripts/openwrt/build.sh
 
+rebuild: ## Rebuild OpenWrt (DIRTY but quicker)
+	@./scripts/openwrt/build.sh --rebuild
+
 qemu: ## Run built aarch64 files in QEMU (Quit: CTRL+A, then X)
 	@./scripts/openwrt/qemu.sh
 
--- scripts/openwrt/build.sh
+++ scripts/openwrt/build.sh
@@ -3,14 +3,52 @@
 BASE_SCRIPTS_DIR=$(dirname "$0")/..
 . "$BASE_SCRIPTS_DIR/common.sh"
 
+print_help()
+{
+    printf "Usage: build.sh\n"
+    printf "This script builds OpenWrt incl. pre-required commands\n\n"
+
+    printf "  --rebuild    Dirty but quicker build\n"
+    printf "  --help       Show this help menu\n"
+}
+
+unset rebuild
+while :; do
+    case $1 in
+        -\?|-help|--help)
+            print_help
+            exit 0
+            ;;
+        --rebuild)
+            rebuild=1
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -?*)
+            warnmsg 'Ignored unknown parameter: %s\n' "$1"
+            ;;
+        *)
+            break
+    esac
+    shift
+done
+
 vps_root_dir=$(rootdir)
 
 cd "$vps_root_dir/openwrt" || { errormsg "could not cd into openwrt directory"; exit 1; }
 
-# Create .config from minimal diffconfig
-make defconfig
-# "good practice (...) to ensure quality builds" https://web.archive.org/web/20250602152152/https://openwrt.org/docs/guide-developer/toolchain/use-buildsystem#cleaning_up
-make clean
+if [ -n "$rebuild" ]; then
+    board=$(sed -n 's/^CONFIG_TARGET_BOARD="\(.*\)"/\1/p' .config)
+    target=$(sed -n 's/^CONFIG_TARGET_SUBTARGET="\(.*\)"/\1/p' .config)
+    rm "bin/targets/$board/$target"/openwrt-*
+else
+    # Create .config from minimal diffconfig
+    make defconfig
+    # "good practice (...) to ensure quality builds" https://web.archive.org/web/20250602152152/https://openwrt.org/docs/guide-developer/toolchain/use-buildsystem#cleaning_up
+    make clean
+fi
 
 ncpus=$(nproc)
 make "-j$ncpus"
